d5f0a2ab16a4c032b0eaa89a3dfb9a9822a8b9d7
John Matter <matter@virginia.edu>
2021-09-28 14:59:38 -0400

    add beta slurm scripts

---
+++ slurm/generate_preprocess.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# patients=(BE CT DA FJ HD HI LJ MB MJ1 MK PJ2 PP SB WJ LK TC KS LL DJ)
+# patients=(MB HI WT BB1 GB)
+patients=(LJ)
+timepoints=(Pre Post1)
+
+for patient in ${patients[@]}; do
+    for timepoint in ${timepoints[@]}; do
+        patient_dir=${patient}\\/${timepoint}
+        patient_str=${patient}_${timepoint}
+        filename=preprocess_${patient_str}.slurm
+
+        cat ./preprocess_template_slurm.txt | \
+        sed -e "s/PATIENTSTR/${patient_str}/" \
+            > $filename
+
+    done
+done
---
+++ slurm/generate_radiomics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# patients=(BE CT DA FJ HD HI LJ MB MJ1 MK PJ2 PP SB WJ LK TC KS LL DJ)
+patients=(MB HI WT BB1 GB)
+
+for patient in ${patients[@]}; do
+    patient_dir=${patient}
+    patient_str=${patient}
+    filename=radiomics_${patient_str}.slurm
+
+    cat ./radiomics_template_slurm.txt | \
+    sed -e "s/PATIENTSTR/${patient_str}/" \
+        > $filename
+done
