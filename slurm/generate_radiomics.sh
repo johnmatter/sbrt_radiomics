c83cfed9c8c6d5fb0e59e14d8e235e44eed56f24
John Matter <matter@virginia.edu>
2021-09-28 16:22:29 -0400

    fix radiomics slurm script bug

--- slurm/generate_radiomics.sh
+++ slurm/generate_radiomics.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # patients=(BE CT DA FJ HD HI LJ MB MJ1 MK PJ2 PP SB WJ LK TC KS LL DJ)
-patients=(MB HI WT BB1 GB)
+# patients=(MB HI WT BB1 GB)
+patients=(LJ)
+timepoints=(Pre Post1)
 
 for patient in ${patients[@]}; do
-    patient_dir=${patient}
-    patient_str=${patient}
-    filename=radiomics_${patient_str}.slurm
+    for timepoint in ${timepoints[@]}; do
+        patient_dir=${patient}\\/${timepoint}
+        patient_str=${patient}_${timepoint}
+        filename=radiomics_${patient_str}.slurm
 
-    cat ./radiomics_template_slurm.txt | \
-    sed -e "s/PATIENTSTR/${patient_str}/" \
-        -e "s/PATIENTDIR/${patient_dir}/" \
-        > $filename
+        cat ./radiomics_template_slurm.txt | \
+        sed -e "s/PATIENTSTR/${patient_str}/" \
+            -e "s/PATIENTDIR/${patient_dir}/" \
+            > $filename
+    done
 done
